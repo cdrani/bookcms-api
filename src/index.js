a4405473ed5d16c01108221de464cd1bc9726898
cdrainxv <cdrainxv@users.noreply.github.com>
2018-12-08 08:52:33 -0700

    setup apollo server with express

--- src/index.js
+++ src/index.js
@@ -1,13 +1,75 @@
-import 'dotenv/config';
+import cors from 'cors'
+import express from 'express'
+import { ApolloServer, gql } from 'apollo-server-express'
 
-const userCredentials = { firstname: 'Robin' };
-const userDetails = { nationality: 'German' };
+const app = express()
+app.use(cors())
 
-const user = {
-  ...userCredentials,
-  ...userDetails,
-};
+const schema = gql`
+  type Query {
+    me: [User!]!
+    book: [Books!]!
+  }
 
-console.log(user);
+  type User {
+    username: String!
+    email: String!
+    password: String!
+  }
 
-console.log(process.env.SOME_ENV_VARIABLE);
+  type Books {
+    id: ID!
+    author: String!
+    title: String!
+    currentChapter: Int!
+    chapters: Int!
+    currentPage: Int!
+    pages: Int!
+  }
+`
+
+const resolvers = {
+  Query: {
+    me: () => [
+      {
+        username: 'spinelli',
+        email: 'spinell@gmail.com',
+        password: 'spinellitortellini'
+      },
+      {
+        username: 'vlassel',
+        email: 'spinell@gmail.com',
+        password: 'lasseltassel'
+      }
+    ],
+    book: () => [
+      {
+        author: 'Ashley Spinell',
+        title: '4th Grade War Stories',
+        currentChapter: 5,
+        chapters: 12,
+        currentPage: 79,
+        pages: 231
+      },
+      {
+        author: 'Vince Lassel',
+        title: 'School Legend',
+        currentChapter: 8,
+        chapters: 15,
+        currentPage: 153,
+        pages: 275
+      }
+    ]
+  }
+}
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers
+})
+
+server.applyMiddleware({ app, path: '/graphql' })
+
+app.listen({ port: 8000 }, () => {
+  console.log('Apollo Server on https://localhost:8000/graphql')
+})
