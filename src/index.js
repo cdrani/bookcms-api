43dd52da1581e40b8c2d10a92fcd10dbf151945c
cdrainxv <cdrainxv@users.noreply.github.com>
2018-12-08 20:19:54 -0700

    setup seed data with users and books

--- src/index.js
+++ src/index.js
@@ -17,7 +17,54 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app, path: '/graphql' })
 
-sequelize.sync().then(async () => {
+const eraseDatabaseOnSync = true
+
+const createUsersWithBooks = async () => {
+  await models.User.create(
+    {
+      username: 'spinelli',
+      email: 'spinelli@gmail.com',
+      password: 'spinellitortellini',
+      books: [
+        {
+          author: 'Vince Lassel',
+          title: 'Vicenzo',
+          currentChapter: 8,
+          chapters: 15,
+          currentPage: 153,
+          pages: 315
+        }
+      ]
+    },
+    { include: [models.Book] }
+  )
+
+  await models.User.create(
+    {
+      username: 'vlassel',
+      email: 'vlasses@gmail.com',
+      password: 'lasseltassel',
+      books: [
+        {
+          author: 'Ashley Spinelli',
+          title: 'Spin Cycle',
+          currentChapter: 9,
+          chapters: 12,
+          currentPage: 210,
+          pages: 275
+        }
+      ]
+    },
+    { include: [models.Book] }
+  )
+}
+
+// remove force option prior to deploy
+sequelize.sync({ force: eraseDatabaseOnSync }).then(async () => {
+  if (eraseDatabaseOnSync) {
+    createUsersWithBooks()
+  }
+
   app.listen({ port: 8000 }, () => {
     console.log('Apollo Server on https://localhost:8000/graphql')
   })
