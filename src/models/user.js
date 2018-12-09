e85c5423f100024583ec4059c0788803abc85d23
cdrainxv <cdrainxv@users.noreply.github.com>
2018-12-08 18:53:22 -0700

    setup user model

---
+++ src/models/user.js
@@ -0,0 +1,15 @@
+const user = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    username: { type: DataTypes.STRING },
+    email: { type: DataTypes.STRING },
+    password: { type: DataTypes.STRING }
+  })
+
+  User.associate = models => {
+    User.hasMany(models.Books, { onDelete: 'CASCADE' })
+  }
+
+  return User
+}
+
+export default user
