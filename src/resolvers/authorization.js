cb217d0c25b86023034335f0ec482edd57b847b1
cdrainxv <cdrainxv@users.noreply.github.com>
2018-12-10 14:04:00 -0700

    verify authenticated user isBookOwner

--- src/resolvers/authorization.js
+++ src/resolvers/authorization.js
@@ -3,3 +3,11 @@ import { skip } from 'graphql-resolvers'
 
 export const isAuthenticated = (_root, _args, { me }) =>
   me ? skip : new ForbiddenError('Not authenticated as user.')
+
+export const isBookOwner = async (_root, { id }, { me, models: { Book } }) => {
+  const book = await Book.findById(id, { raw: true })
+  if (book.userId !== me.id) {
+    throw new ForbiddenError('Not authenticated as owner.')
+  }
+  return skip
+}
--- src/resolvers/book.js
+++ src/resolvers/book.js
@@ -1,5 +1,5 @@
 import { combineResolvers } from 'graphql-resolvers'
-import { isAuthenticated } from './authorization'
+import { isAuthenticated, isBookOwner } from './authorization'
 
 export default {
   Query: {
@@ -28,20 +28,22 @@ export default {
         })
     ),
 
-    deleteBook: async (_root, { input: { id } }, { models: { Book } }) =>
-      await Book.destroy({ where: { id } }),
+    deleteBook: combineResolvers(
+      isBookOwner,
+      async (_root, { input: { id } }, { models: { Book } }) =>
+        await Book.destroy({ where: { id } })
+    ),
 
-    updateBookTitle: async (
-      _root,
-      { input: { id, newTitle } },
-      { models: { Book } }
-    ) => {
-      const book = await Book.findById(id)
-      return book.update(
-        { title: newTitle },
-        { where: { id: id }, returning: true }
-      )
-    }
+    updateBookTitle: combineResolvers(
+      isBookOwner,
+      async (_root, { input: { id, newTitle } }, { models: { Book } }) => {
+        const book = await Book.findById(id)
+        return book.update(
+          { title: newTitle },
+          { where: { id: id }, returning: true }
+        )
+      }
+    )
   },
 
   Book: {
