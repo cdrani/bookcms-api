7e1c45e07408286d2855cd3d7f3ebdf2b67779a3
cdrainxv <cdrainxv@users.noreply.github.com>
2018-12-10 21:21:02 -0700

    test user found query

--- src/tests/user.spec.js
+++ src/tests/user.spec.js
@@ -1,7 +1,20 @@
 import { expect } from 'chai'
+import * as userApi from './api'
 
 describe('users', () => {
-  it('user is user', () => {
-    expect('user').to.eql('user')
+  describe('user($id: String!): User', () => {
+    it('returns a user when a user is found', async () => {
+      const expectedResult = {
+        data: {
+          user: {
+            id: '1',
+            username: 'spinelli',
+            email: 'spinelli@gmail.com'
+          }
+        }
+      }
+      const result = await userApi.user({ id: '1' })
+      expect(result.data).to.eql(expectedResult)
+    })
   })
 })
